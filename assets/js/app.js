2fafbb01e2eb542a7ce69060bdd35b5180dfd61f
Mohit <mkjakhar909@gmail.com>
2023-02-17 16:11:44 +0530

    finals

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,3 +32,36 @@ $('.slider').slick({
         }
     ]
 });
+// second-slider
+
+$(".my-slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+    nextArrow: ".nex-arrow",
+    prevArrow: ".pre-arrow",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
+// slider with counter
+$(function () {
+    $('.custom-slider').on('init', function (event, slick) {
+            $(this).append('<div class="slick-counter"><span class="current"></span> / <span class="total"></span></div>');
+            $('.current').text(slick.currentSlide + 01);
+            $('.total').text(slick.slideCount);
+        })
+        .slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            arrows: true,
+            nextArrow: ".custom-next",
+            prevArrow: ".custom-prev",
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        })
+        .on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+            $('.current').text(nextSlide + 01);
+        });
+});
